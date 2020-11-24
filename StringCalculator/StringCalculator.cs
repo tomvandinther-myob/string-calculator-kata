7e7e54c9a343a0a8ae9a4ebdf59e4ea07bdad5f5
tomvandinther-myob <tom.vandinther@myob.com>
2020-11-24 14:11:52 +1300

    step 3 passed

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -1,14 +1,21 @@
 ﻿using System;
+using System.Linq;
 
 namespace StringCalculator
 {
     public class StringCalculator
     {
+
+        private int[] ParseInts(string calcString)
+        {
+            return calcString.Split(",").Select(int.Parse).ToArray();
+        }
+        
 		public int Add(string calcString)
         {
-            return int.Parse(calcString);
+            return ParseInts(calcString).Sum();
 
-			throw new NotImplementedException("Not implemented.");
+            throw new NotImplementedException("Not implemented.");
         }
     }
 }
