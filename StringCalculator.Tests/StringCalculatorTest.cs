10fe7921981c05263e85ce3fec62d3ab4489eaab
tomvandinther-myob <tom.vandinther@myob.com>
2020-11-24 14:15:38 +1300

    step 5 failed

--- StringCalculator.Tests/StringCalculatorTest.cs
+++ StringCalculator.Tests/StringCalculatorTest.cs
@@ -35,5 +35,15 @@ namespace StringCalculator.UnitTests
 
 			Assert.Equal(expectedResult, result);
 		}
+		
+		[Theory]
+		[InlineData("1,2\n3", 1+2+3)]
+		[InlineData("\n\n3,9", 3+5+3+9)]
+		public void Add_XNumbers_WithLineBreaks_ShouldReturnSum(string value, int expectedResult)
+		{
+			var result = _stringCalculator.Add(value);
+
+			Assert.Equal(expectedResult, result);
+		}
 	}
 }
